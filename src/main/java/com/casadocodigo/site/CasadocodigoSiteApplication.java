9c1321b9484cd6f12c0de739429b1638b2fac9ad
Henrique Lopes <henriquelopees7@gmail.com>
2021-03-23 07:09:13 -0300

    Initial commit

---
+++ src/main/java/com/casadocodigo/site/CasadocodigoSiteApplication.java
@@ -0,0 +1,13 @@
+package com.casadocodigo.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadocodigoSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasadocodigoSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casadocodigo/site/CasadocodigoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casadocodigo.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
