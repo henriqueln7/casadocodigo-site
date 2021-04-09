6c00fd5c161e0b66aee757f02b00f80449f248dd
Henrique Lopes <henriquelopees7@gmail.com>
2021-04-09 15:37:05 -0300

    Refatora NovoAutorEmailUnicoValidatorTest para nomes mais descritivos no testes e import estático do Mockito

--- src/test/java/com/casadocodigo/site/cadastroautor/NovoAutorEmailUnicoValidatorTest.java
+++ src/test/java/com/casadocodigo/site/cadastroautor/NovoAutorEmailUnicoValidatorTest.java
@@ -2,7 +2,6 @@ package com.casadocodigo.site.cadastroautor;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 
@@ -10,21 +9,23 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class NovoAutorEmailUnicoValidatorTest {
 
     @Test
-    @DisplayName("Deve adicionar um erro ao objeto Errors do spring quando o email do autor ja esta cadastrado")
-    void deveAdicionarUmErroAoObjetoErrorsDoSpringQuandoOEmailDoAutorJaEstaCadastrado() {
+    @DisplayName("Deve armazenar erro quando o email do autor ja esta cadastrado")
+    void deveArmazenarErroQuandoOEmailDoAutorJaEstaCadastrado() {
         String emailExistente = "henrique@email.com";
 
         NovoAutorForm novoAutorForm = new NovoAutorForm("Jose", emailExistente, "descricao legal");
 
-        AutorRepository autorRepository = Mockito.mock(AutorRepository.class);
+        AutorRepository autorRepository = mock(AutorRepository.class);
         NovoAutorEmailUnicoValidator novoAutorEmailUnicoValidator = new NovoAutorEmailUnicoValidator(autorRepository);
 
         Autor autorExistente = new Autor("Henrique", emailExistente, "Descricao pipipi");
-        Mockito.when(autorRepository.findByEmail(emailExistente)).thenReturn(Optional.of(autorExistente));
+        when(autorRepository.findByEmail(emailExistente)).thenReturn(Optional.of(autorExistente));
 
         Errors errors = new BeanPropertyBindingResult(novoAutorForm, "novoAutorForm");
         novoAutorEmailUnicoValidator.validate(novoAutorForm, errors);
@@ -33,16 +34,16 @@ class NovoAutorEmailUnicoValidatorTest {
     }
 
     @Test
-    @DisplayName("Nao deve adicionar um erro ao objeto Errors do spring se o email do autor nao esta cadastrado")
-    void naoDeveAdicionarUmErroAoObjetoErrorsDoSpringSeOEmailDoAutorNaoEstaCadastrado() {
+    @DisplayName("Nao deve armazenar erro se o email do autor nao esta cadastrado")
+    void naoDeveArmazenarErroSeOEmailDoAutorNaoEstaCadastrado() {
         String emailNaoExistente = "henrique@email.com";
 
         NovoAutorForm novoAutorForm = new NovoAutorForm("Jose", emailNaoExistente, "descricao legal");
 
-        AutorRepository autorRepository = Mockito.mock(AutorRepository.class);
+        AutorRepository autorRepository = mock(AutorRepository.class);
         NovoAutorEmailUnicoValidator novoAutorEmailUnicoValidator = new NovoAutorEmailUnicoValidator(autorRepository);
 
-        Mockito.when(autorRepository.findByEmail(emailNaoExistente)).thenReturn(Optional.empty());
+        when(autorRepository.findByEmail(emailNaoExistente)).thenReturn(Optional.empty());
 
         Errors errors = new BeanPropertyBindingResult(novoAutorForm, "novoAutorForm");
         novoAutorEmailUnicoValidator.validate(novoAutorForm, errors);
