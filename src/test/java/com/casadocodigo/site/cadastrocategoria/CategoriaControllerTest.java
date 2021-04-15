84dce7a8c07fed64b033f27d059b140b10b0ced4
Henrique Lopes <henriquelopees7@gmail.com>
2021-04-15 15:55:38 -0300

    Adiciona testes integrados em CategoriaController

---
+++ src/test/java/com/casadocodigo/site/cadastrocategoria/CategoriaControllerTest.java
@@ -0,0 +1,56 @@
+package com.casadocodigo.site.cadastrocategoria;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+class CategoriaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Deve mostrar form para cadastrar categoria")
+    void deveMostrarFormParaCadastrarCategoria() throws Exception {
+        mockMvc.perform(get("/categorias/novo"))
+               .andExpect(status().isOk())
+               .andExpect(view().name("categoria/novo"));
+    }
+
+    @Test
+    @DisplayName("Deve cadastrar categoria e retornar para a pagina de form")
+    void deveCadastrarCategoriaERetornarParaAPaginaDeForm() throws Exception {
+        mockMvc.perform(post("/categorias")
+                                .contentType(APPLICATION_FORM_URLENCODED)
+                                .param("nome", "AAAAAAAA"))
+               .andExpect(status().is3xxRedirection())
+               .andExpect(header().stringValues("Location", "/categorias/novo"));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Deve redirecionar para a pagina de form com erros se o nome da categoria passado eh invalido")
+    @NullSource
+    @ValueSource(strings = {"", "   "})
+    void deveRedirecionarParaAPaginaDeFormComErrosSeONomeDaCategoriaPassadoEhInvalido(String nomeCategoria) throws Exception {
+        mockMvc.perform(post("/categorias")
+                                .contentType(APPLICATION_FORM_URLENCODED)
+                                .param("nome", nomeCategoria))
+               .andExpect(view().name("categoria/novo"))
+               .andExpect(model().hasErrors());
+    }
+}
