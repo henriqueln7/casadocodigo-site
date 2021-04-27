1416bb429d10c45d3bb695fc671f806e2ad04e81
Henrique Lopes <henriquelopees7@gmail.com>
2021-04-26 21:45:22 -0300

    Refatora testes utilizando o @WebMvc e mockando CategoriaRepository

--- src/test/java/com/casadocodigo/site/cadastrocategoria/CategoriaControllerTest.java
+++ src/test/java/com/casadocodigo/site/cadastrocategoria/CategoriaControllerTest.java
@@ -6,23 +6,26 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureTestDatabase
+@WebMvcTest(CategoriaController.class)
 class CategoriaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private CategoriaRepository categoriaRepository;
 
     @Test
     @DisplayName("Deve mostrar form para cadastrar categoria")
@@ -37,9 +40,11 @@ class CategoriaControllerTest {
     void deveCadastrarCategoriaERetornarParaAPaginaDeForm() throws Exception {
         mockMvc.perform(post("/categorias")
                                 .contentType(APPLICATION_FORM_URLENCODED)
-                                .param("nome", "AAAAAAAA"))
+                                .param("nome", "Nova categoria"))
                .andExpect(status().is3xxRedirection())
                .andExpect(header().stringValues("Location", "/categorias/novo"));
+
+        verify(categoriaRepository).save(new Categoria("Nova categoria")); // Aqui não utilizei o any(Categoria.class) para me garantir que a categoria passada como argumento possui o mesmo nome que passei na requisição.
     }
 
     @ParameterizedTest
@@ -52,18 +57,18 @@ class CategoriaControllerTest {
                                 .param("nome", nomeCategoria))
                .andExpect(view().name("categoria/novo"))
                .andExpect(model().hasErrors());
+
+        verify(categoriaRepository, never()).save(any(Categoria.class));
     }
 
     @Test
     @DisplayName("Nao deve cadastrar categoria se o nome eh duplicado")
     void naoDeveCadastrarCategoriaSeONomeEhDuplicado() throws Exception {
         String nomeCategoriaDuplicado = "Mobile";
-        // Esse deve ser cadastrado normalmente
-        mockMvc.perform(post("/categorias")
-                                .contentType(APPLICATION_FORM_URLENCODED)
-                                .param("nome", nomeCategoriaDuplicado))
-               .andReturn();
-        // Esse deve dar erro pois a categoria "Mobile" já foi cadastrada
+
+        when(categoriaRepository.findByNome(nomeCategoriaDuplicado)).thenReturn(Optional.of(new Categoria(nomeCategoriaDuplicado)));
+
+        // Esse deve dar erro pois a categoria "Mobile" já foi cadastrada no BD
         mockMvc.perform(post("/categorias")
                                 .contentType(APPLICATION_FORM_URLENCODED)
                                 .param("nome", nomeCategoriaDuplicado))
